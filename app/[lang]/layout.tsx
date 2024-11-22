7c6d5e78fac09296a23c6c8886c7d780213813b4
Maurício Bruno <mb.bruno.s@gmail.com>
2024-11-22 18:53:04 -0300

    feat: complete pt-br ui translations

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -2,7 +2,7 @@ import "./global.css";
 import { RootProvider } from "fumadocs-ui/provider";
 import { Inter } from "next/font/google";
 import type { ReactNode } from "react";
-import { I18nProvider } from "fumadocs-ui/i18n";
+import { I18nProvider, Translations } from "fumadocs-ui/i18n";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -16,6 +16,21 @@ export default async function Layout(props: {
 
   const { children } = props;
 
+  const translations: Record<string, Partial<Translations>> = {
+    'pt-br': {
+      chooseTheme: "Escolha o tema",
+      chooseLanguage: "Escolha o idioma",
+      editOnGithub: "Editar no GitHub",
+      lastUpdate: "Última atualização",
+      nextPage: "Próximo",
+      previousPage: "Anterior",
+      search: "Pesquisar",
+      searchNoResult: "Nenhum resultado encontrado",
+      toc: "Nesta página",
+      tocNoHeadings: "Nenhum cabeçalho encontrado",
+    }
+  }
+
   return (
     <html lang="pt-br" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
@@ -25,11 +40,7 @@ export default async function Layout(props: {
             { locale: "pt-br", name: "Português (Brasil)" },
             { locale: "en", name: "English" },
           ]}
-          translations={{
-            'pt-br': {
-              chooseLanguage: "Escolha o idioma",
-            }
-          }[params.lang]}
+          translations={translations[params.lang]}
         >
           <RootProvider>{children}</RootProvider>
         </I18nProvider>
